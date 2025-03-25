32acefb01ae35d930aa22c348f977e37954c01fd
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-03-25 19:43:48 -0400

    Outlined constraints

--- solver.py
+++ solver.py
@@ -4,4 +4,8 @@ from queens import QueensGame
 
 
 def solve_game(game: QueensGame):
-    pass
+    p = LpProblem("Solve Queens Game")
+    # Each row must have exactly 1 queen
+    # Each column must have exactly 1 queen
+    # Each color must have exactly 1 queen
+    # Queens cannot be adjacent (including diagonal) of each other
