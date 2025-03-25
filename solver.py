eecec5889811eadee7583aac266bf898fb04bca8
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-03-25 19:49:26 -0400

    Started constraints for 1 queen per row and column

--- solver.py
+++ solver.py
@@ -1,11 +1,21 @@
-from pulp import LpProblem, LpVariable, lpSum
+from pulp import LpProblem, LpVariable, LpBinary
 
 from queens import QueensGame
 
 
 def solve_game(game: QueensGame):
     p = LpProblem("Solve Queens Game")
+
+    board = LpVariable.dicts("board", game.grid, cat=LpBinary)
+
     # Each row must have exactly 1 queen
+    for row in range(game.size):
+        pass
+
     # Each column must have exactly 1 queen
+    for col in range(game.size):
+        pass
+
     # Each color must have exactly 1 queen
+
     # Queens cannot be adjacent (including diagonal) of each other
